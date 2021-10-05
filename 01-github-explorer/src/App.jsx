6332618ba0152ab5e18e1c1f57177f2cfd7c1f09
jadirf31 <jadirf31@gmail.com>
2021-10-05 12:23:16 +0100

    Listando repositórios

--- 01-github-explorer/src/App.jsx
+++ 01-github-explorer/src/App.jsx
@@ -1,12 +1,8 @@
-import { Counter } from './components/Counter';
 import { RepositoryList } from './components/RepositoryList';
 import './syles/global.scss';
 
 export function App() {
     return (
-        <>
-            <RepositoryList/>
-            <Counter/>
-        </>
+        <RepositoryList/>
     )
 }
